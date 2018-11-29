82e096436f6b289be9b2223eeb102cfe07d3b984
Nick Penkov <nick.penkov@gmail.com>
2018-11-29 13:05:09 -0800

    Change hello message

--- main.go
+++ main.go
@@ -14,7 +14,7 @@ func main() {
 
 		f := fib()
 
-		res := &response{Message: "Hello World"}
+		res := &response{Message: "Hello World, my friend"}
 
 		for _, e := range os.Environ() {
 			pair := strings.Split(e, "=")
